1ec8f31cdd4662afb71f3e6b608acccc27cf447c
TommyYuan0215 <88966791+TommyYuan0215@users.noreply.github.com>
2022-10-28 14:24:45 +0800

    Add files via upload

---
+++ JS/faq.js
@@ -0,0 +1,152 @@
+		function showgfaq(divId, element)
+		{
+			document.getElementById(divId).style.display = element.value == 1 ? 'block' : 'none';
+		}
+		
+		function showsrfaq(divId, element)
+		{
+			document.getElementById(divId).style.display = element.value == 2 ? 'block' : 'none';
+		}
+		
+		function showocsfaq(divId, element)
+		{
+			document.getElementById(divId).style.display = element.value == 3 ? 'block' : 'none';
+		}
+		
+		function showprefaq(divId, element)
+		{
+			document.getElementById(divId).style.display = element.value == 4 ? 'block' : 'none';
+		}
+		
+		function showpostfaq(divId, element)
+		{
+			document.getElementById(divId).style.display = element.value == 5 ? 'block' : 'none';
+		}
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		$(document).ready(function(){
+		  $("#card1").click(function(){
+			$("#card_1").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card2").click(function(){
+			$("#card_2").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card3").click(function(){
+			$("#card_3").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card4").click(function(){
+			$("#card_4").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card5").click(function(){
+			$("#card_5").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card6").click(function(){
+			$("#card_6").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card7").click(function(){
+			$("#card_7").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card8").click(function(){
+			$("#card_8").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card9").click(function(){
+			$("#card_9").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card10").click(function(){
+			$("#card_10").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card11").click(function(){
+			$("#card_11").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card12").click(function(){
+			$("#card_12").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card13").click(function(){
+			$("#card_13").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card14").click(function(){
+			$("#card_14").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card15").click(function(){
+			$("#card_15").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card16").click(function(){
+			$("#card_16").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card17").click(function(){
+			$("#card_17").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card18").click(function(){
+			$("#card_18").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card19").click(function(){
+			$("#card_19").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card20").click(function(){
+			$("#card_20").slideToggle("medium");
+		  });
+		});
+		
+		$(document).ready(function(){
+		  $("#card21").click(function(){
+			$("#card_21").slideToggle("medium");
+		  });
+		});
+		
